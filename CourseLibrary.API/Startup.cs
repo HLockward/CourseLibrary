01d970f1c4337ab305e1fa5dad5d8edb0c328ca6
Harold Lockward <HLockward12@gmail.com>
2020-11-09 21:02:41 +0100

    added a new form to access the connection string from appsettings

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,15 +17,21 @@ namespace CourseLibrary.API
 {
     public class Startup
     {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<CoursesContext>(options =>
-                options.UseSqlServer(
-                @"Server=localhost,1433;Database=CoursesDB;Persist Security Info=False;User ID=sa;Password=HLockward2018;Trusted_Connection=False;"
-                )
+            var connectionString = Configuration["ConnectionStrings:CoursesDBConnectionString"];
+            services.AddDbContext<CoursesContext>(o =>
+                o.UseSqlServer(connectionString)
             );
 
             services.AddScoped<ICourseRepository, CourseRepository>();
